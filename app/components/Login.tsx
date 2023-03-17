5a133d775a218ef3ad1e734a8cd3f77c1959d722
chiranjeebhub <chiranjib.jena@gmail.com>
2023-03-17 08:04:35 +0530

    pocketbase setup is done

---
+++ app/components/Login.tsx
@@ -0,0 +1,12 @@
+const Login = () => {
+  return (
+    <div className="flex justify-center items-center h-screen bg-gray-200">
+      <div className="bg-white w-4/12 h-400 mt-10 p-4 rounded-lg shadow-lg flex flex-col items-center">
+        <h1 className="text-xl font-semibold">You must Sign In to join</h1>
+        <p className="text-sm text-gray-400">Welcome to Chiranjeeb's Blog</p>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
